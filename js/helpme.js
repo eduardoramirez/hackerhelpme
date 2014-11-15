a9ec749041dcc9a192184834611ce91d86e74136
Eduardo Ramirez <eduardo@ucsd.edu>
2014-11-14 23:34:37 -0800

    more functions

--- js/helpme.js
+++ js/helpme.js
@@ -1,10 +1,29 @@
 var fb = new Firebase('https://hacknighthelpme.firebaseio.com');
 
+var users = [];
+
+$("submit").click(function(){
+  var name = $('#name').val();
+  var location = $('#location').val();
+
+  var field = '';
+
+  for(var i = 0; i < 6; i++)
+  {
+    if($('#checkbox'+i.toString()).is(':checked')){
+      field += $('#checkbox'+i.toString()).val() + ' ';
+    }
+  }
+
+  newHelp(name, location, field);
+
+});
 
 function newHelp(name, location, field){
 
   if(name && location && field) {
     var newHelpRef = fb.push({'user': name, 'location':location, 'field': field});
+    users.push({user: name, loc: newHelpRef});
   }
 };
 
@@ -33,6 +52,6 @@ function displayQueue(userinfo){
 };
 
 
-function removeUser(){
+function removeUser(user){
 
 };
